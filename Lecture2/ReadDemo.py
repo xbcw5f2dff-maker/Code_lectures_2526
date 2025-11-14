f1f5b050d8f0b589b7c01f606f18ccba4acb0177
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-14 13:08:42 +0100

    Fix print statement syntax and formatting

--- Lecture2/ReadDemo.py
+++ Lecture2/ReadDemo.py
@@ -1,13 +1,13 @@
 def main():
     # Open file for input
     inputFile = open("Presidents.txt", "r")
-    print("(1) Using read(): ")
-    print(inputFile.read()) # Read all in the file
+    print("(1) Using read(): ") 
+    print(inputFile.read()) # Read all in the file 
     inputFile.close() # Close the input file
 
     # Open file for input
     inputFile = open("Presidents.txt", "r")
-    print("\n(2) Using read(number): ")
+    print("\n(2) Using read(number): ") #print een lege regel en dan (2) met aantal nummers tekst 
     s1 = inputFile.read(5)
     print(s1)
     s2 = inputFile.read(15) # Read 15 characters to s2
@@ -21,7 +21,7 @@ def main():
     line2 = inputFile.readline()
     line3 = inputFile.readline()
     line4 = inputFile.readline()
-    print(repr(line1))
+    print(repr(line1) 
     print(repr(line2))
     print(repr(line3))
     print(repr(line4))
