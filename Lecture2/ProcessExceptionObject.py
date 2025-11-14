32284c1499e381791944178e8113fc18cf89c6c4
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-14 16:32:45 +0100

    Update ProcessExceptionObject.py

--- Lecture2/ProcessExceptionObject.py
+++ Lecture2/ProcessExceptionObject.py
@@ -1,7 +1,7 @@
 try:
     number = float(input("Enter a number: "))
     print("The number entered is", number)
-except ValueError as ex:
+except ValueError as ex: #valueerror; number is tekst en je geeft die error de naam ex zodat je bij de print("excpetion:" dus dat is lett tekst, als je dan ex zet dan zal hij nog zeggen could not convert string to float: drie
     print("Exception:", ex)
 
 
