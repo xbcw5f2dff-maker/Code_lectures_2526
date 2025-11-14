f620202320986016246b2a5e894a1a1c37793ee9
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-14 14:24:39 +0100

    Improve exception handling in TestException.py
    Refactor exception handling in TestException.py to clarify functionality.

--- Lecture2/TestException.py
+++ Lecture2/TestException.py
@@ -2,7 +2,7 @@ def main():
     try:
         number1 = int(input("Enter an integer: "))      
         number2 = int(input("Enter an integer: "))      
-        result = number1 / number2
+        result = number1 / number2 #als dit geen probleem geeft dan springt het naar else, wel probleem => 1 van de excepts wordt uitgevoerd 
         print("Result is " + str(result))
     except ZeroDivisionError: # Catch zero divisor error
         print("Division by zero!")
@@ -10,7 +10,7 @@ def main():
         print("Something wrong in the input")
     else:
         print("No exceptions")
-    finally:
+    finally: #wordt altijd uitgevoerd 
         print("The finally clause is executed")
 
 main()
