cbcd636ba1c7c9be3720f73066da0b61e122fdc0
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-14 18:13:51 +0100

    Update CountOccurrenceOfWordsFromFile.py

--- Lecture2/CountOccurrenceOfWordsFromFile.py
+++ Lecture2/CountOccurrenceOfWordsFromFile.py
@@ -8,27 +8,28 @@ def main():
         processLine(line.lower(), wordCounts)
     inputFile.close() 
     
-    pairs = list(wordCounts.items()) # Get pairs from the dictionary   
+    pairs = list(wordCounts.items()) # Get pairs from the dictionary, wordcounts.items() geeft de (key, value)-paren als tuples, list() om lijst vn te maken vande paren tuples 
 
-    items = [[x, y] for (y, x) in pairs] # Reverse pairs in the list
-
+    items = [[x, y] for (y, x) in pairs] # Reverse pairs in the list, want het begint met woord en dan aantal en lijsten worden gesorteerd obv 1ste element vh paar, en we willen op aantal sorteren ipv op woord
+    #resultaat; [aantal, woord] lijstenwat
     items.sort() # Sort pairs in items
 
-    for i in range(len(items) - 1, 0, -1):
-        print(items[i][1] + "\t" + str(items[i][0]))
-  
+    for i in range(len(items) - 1, 0, -1): #start bij laatste index (de grootste) eindig net boven 0
+        print(items[i][1] + "\t" + str(items[i][0])) #items[i][1] => woord, items[i][0] => aantal en backslash t is tab
+                                                     #items[i][0] => 1ste element vd lijst => aantal
+                                                     #items[i][1] => 2 de element vd lijst => woord 
 # Count each word in the line
 def processLine(line, wordCounts): 
     line = replacePunctuations(line) # Replace punctuations with space
-    words = line.split() # Get words from each line
+    words = line.split() # Get words from each line, splitst woorden op spaties => resultaat is lijst van worden => .split() geeft ALTIJD lijst terug
     for word in words:
         if word in wordCounts:
-            wordCounts[word] += 1
+            wordCounts[word] += 1 #als woord al bestaat => teller + 1
         else:
-            wordCounts[word] = 1
+            wordCounts[word] = 1 #anders => eerste keer gezien => teller = 1
 
 # Replace punctuations in the line with space
-def replacePunctuations(line):
+def replacePunctuations(line): #elke letter ch controleren als het een leesteken is vervangen door spatie 
     for ch in line:
         if ch in '~@#$%^&*()_-+=~"<>?/,.;!{}[]|':
             line = line.replace(ch, " ")
