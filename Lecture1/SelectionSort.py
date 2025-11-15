91db4dbf8b94b148ed395d96540e366e82e4c16d
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-15 22:00:50 +0100

    Improve comments for index correction in SelectionSort
    Clarified the index correction for finding the minimum element in the selection sort algorithm.

--- Lecture1/SelectionSort.py
+++ Lecture1/SelectionSort.py
@@ -4,7 +4,11 @@ def selectionSort(lst):
         # Find the minimum in the lst[i : len(lst)]
         currentMin = min(lst[i : ])
         currentMinIndex = i + lst[i: ].index(currentMin) #Vind waar dat getal in de originele lijst staat
-        
+                                                                #je zoekt minimum in de slice lst[i:]
+	                                                        	#maar .index() geeft de index binnen die slice, dus die begint bij 0
+		                                                        #daarom moet je + i doen om de index te corrigeren naar de volledige lijst
+
+Dit is waarom je daar i + index nodig hebt.
         # Swap lst[i] with lst[currentMinIndex] if necessary
         if currentMinIndex != i:
             lst[currentMinIndex], lst[i] = lst[i], currentMin
