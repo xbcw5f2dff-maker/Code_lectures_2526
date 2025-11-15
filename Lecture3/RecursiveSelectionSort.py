047a708dd9e4fe8f9f07bf322899cde69e199a71
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-15 21:57:50 +0100

    Enhance comments in RecursiveSelectionSort.py
    Added comments to clarify the sorting process and conditions in the recursive selection sort implementation.

--- Lecture3/RecursiveSelectionSort.py
+++ Lecture3/RecursiveSelectionSort.py
@@ -1,12 +1,14 @@
+#hoofdfunctie 
 def sort(lst):
-    sortHelper(lst, 0, len(lst) - 1) # Sort the entire lst
+    sortHelper(lst, 0, len(lst) - 1) # Sort the entire lst (vn index O tot de laatste de lijst sorteren)
 
+#recursieve functie 
 def sortHelper(lst, low, high):
-    if low < high:
+    if low < high: #als low == high dan stopt de functie (stopconditie)
         # Find the smallest number and its index in lst[low .. high]
-        indexOfMin = low;
-        min = lst[low];
-        for i in range(low + 1, high + 1):
+        indexOfMin = low; #je zegt dat min op low staat 
+        min = lst[low]; #je zegt dat het minimum dat je tot nu toe kent het getal op low is 
+        for i in range(low + 1, high + 1): #eerste laat je weg en laatste wil je er wel bij => range(a, b) gaat van a tot b-1 
             if lst[i] < min:
                 min = lst[i]
                 indexOfMin = i
