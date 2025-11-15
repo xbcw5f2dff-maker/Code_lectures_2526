87a316ce33966606cdff045781c4efacd6f940e1
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-15 22:14:44 +0100

    Update RecursiveSelectionSort.py

--- Lecture3/RecursiveSelectionSort.py
+++ Lecture3/RecursiveSelectionSort.py
@@ -10,12 +10,12 @@ def sortHelper(lst, low, high):
         min = lst[low]; #je zegt dat het minimum dat je tot nu toe kent het getal op low is 
         for i in range(low + 1, high + 1): #eerste laat je weg en laatste wil je er wel bij => range(a, b) gaat van a tot b-1 
             if lst[i] < min:
-                min = lst[i]
-                indexOfMin = i
+                min = lst[i] #je zegt oke getal op i is nu het min 
+                indexOfMin = i #index van dat getal i 
 
         # Swap the smallest in lst[low .. high] with lst[low]
-        lst[indexOfMin] = lst[low]
-        lst[low] = min
+        lst[indexOfMin] = lst[low] #dat getal dat nu min is verander je door getal dat op low stond 
+        lst[low] = min #getal op low verander je door dat getal dat min is 
 
         # Sort the remaining lst[low+1 .. high]
         sortHelper(lst, low + 1, high)
