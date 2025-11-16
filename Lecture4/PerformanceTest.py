740ed726eacd365d9768aa1f203232fc3004eff6
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-16 16:48:00 +0100

    Update PerformanceTest.py

--- Lecture4/PerformanceTest.py
+++ Lecture4/PerformanceTest.py
@@ -1,11 +1,11 @@
 import time
-
+#Deze code meet hoelang het duurt om een heel simpele lus (for-lus) uit te voeren voor verschillende groottes van n.
 def getTime(n):
-    startTime = time.time()
-    k = 0
-    for i in range(n):
-        k = k + 5
-    endTime = time.time()
+    startTime = time.time()    #tijd noteren wnr we starten
+    k = 0                      #variabele die we telkens optellen
+    for i in range(n):         #we doen dit n keer 
+        k = k + 5 #simpele bewerking zodat er iets gebeurt, je wilt gwn weten hoelang het duurt om een bewerking n keer uit te voeren, kon bv evengoed k+1 zijn; een kleine bewerking die de computer verplicht om iets te doen 
+    endTime = time.time()      #tijd na afloop noteren                                     
     print("Execution time for n =", n, "is",
         endTime - startTime, "seconds")
 
