760188828e4a18d431b19bc0532f4b0b6476933a
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-16 17:44:12 +0100

    Update InsertionSort.py

--- Lecture4/InsertionSort.py
+++ Lecture4/InsertionSort.py
@@ -3,15 +3,15 @@ def insertionSort(lst):
     for i in range(1, len(lst)):
         # insert lst[i] into a sorted sublist lst[0..i-1] so that
         #   lst[0..i] is sorted.
-        currentElement = lst[i]
-        k = i - 1
-        while k >= 0 and lst[k] > currentElement:
-            lst[k + 1] = lst[k]
-            k -= 1
+        currentElement = lst[i] #element dat we willen sorteren 
+        k = i - 1 #laatste element van reeds gesorteerde zone; bij i = 2 is gesorteerde zone = index 0 en 1 dus k = 1 
+        while k >= 0 and lst[k] > currentElement: #zolang het element links groter is dan currentElement 
+            lst[k + 1] = lst[k]                    # schuif element op index k één plaats naar rechts
+            k -= 1                                 # ga met je vinger één plaats naar links
   
         # Insert the current element into lst[k + 1]
-        lst[k + 1] = currentElement
-
+        lst[k + 1] = currentElement #als k dus niet groter was dan current element dan wordt k + 1 current element en gaan we verder in de for lus naar de volgende i 
+                                    #daar “valt” het element op de juiste plek in het gesorteerde stuk
 def main():
     list = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12]
     insertionSort(list)
