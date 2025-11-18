69886b741fa7681bb2d42040675eabdc4e5fb839
xbcw5f2dff-maker <xbcw5f2dff@privaterelay.appleid.com>
2025-11-18 19:17:16 +0100

    Update BubbleSort.py

--- Lecture4/BubbleSort.py
+++ Lecture4/BubbleSort.py
@@ -12,7 +12,7 @@ def bubbleSort(lst):
                 lst[i] = lst[i + 1]     #op de plaats van het linske getal zet ik nu het rechtse getal
                 lst[i + 1] = temp       #op plaats van rechtse getal zet ik dan het linske getal dat in het doosje zat 
           
-                needNextPass = True # Next pass still needed dus terug naar while-lus 
+                needNextPass = True # Next pass still needed dus terug naar while-lus
 
 def main():
     lst = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12]
